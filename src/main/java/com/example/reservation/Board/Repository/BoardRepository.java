1d5f36d017b9971985cf00e1258a1fbbcd31b6bb
heekyoung2000 <heekyoung2000@naver.com>
2024-01-30 21:38:17 +0900

    feat: 게시판 생성 및 조회 구현

---
+++ src/main/java/com/example/reservation/Board/Repository/BoardRepository.java
@@ -0,0 +1,10 @@
+package com.example.reservation.Board.Repository;
+
+import com.example.reservation.Board.entitiy.BoardEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface BoardRepository extends JpaRepository<BoardEntity,Long> {
+    List<BoardEntity> findAllByOrderByCreatedAtDesc();
+}
---
+++ src/main/java/com/example/reservation/Board/dto/BoardResponseDTO.java
@@ -0,0 +1,46 @@
+package com.example.reservation.Board.dto;
+
+import com.example.reservation.Board.entitiy.BoardEntity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+public class BoardResponseDTO {
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+    private LocalDateTime createdAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public BoardResponseDTO(BoardEntity entity){
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuther();
+        this.createdAt = entity.getCreatedAt();
+    }
+
+}
