e01ad7e3126c659bca900e54cd85f03fe6b0a7b1
heekyoung2000 <heekyoung2000@naver.com>
2024-02-13 18:54:31 +0900

    feat: api gateway 각 서비스 연결 및 security 설정 추가

--- NewsfeedService/src/main/java/com/example/reservation/config/SecurityConfig.java
+++ NewsfeedService/src/main/java/com/example/reservation/config/SecurityConfig.java
@@ -1,19 +1,14 @@
 package com.example.reservation.config;
 
-import com.example.reservation.jwt.JWTUtil;
-import com.example.reservation.jwt.LoginFilter;
-import com.example.reservation.util.RedisUtil;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -22,13 +17,10 @@ public class SecurityConfig {
     //AuthenticationManager가 인자로 받을 AuthenticationConfiguraion 객체 생성자 주입
     private final AuthenticationConfiguration authenticationConfiguration;
     //JWTUtil 주입
-    private final JWTUtil jwtUtil;
-    private final RedisUtil redisUtil;
 
-    public SecurityConfig(AuthenticationConfiguration authenticationConfiguration, JWTUtil jwtUtil, RedisUtil redisUtil) {
+    public SecurityConfig(AuthenticationConfiguration authenticationConfiguration) {
         this.authenticationConfiguration = authenticationConfiguration;
-        this.jwtUtil = jwtUtil;
-        this.redisUtil = redisUtil;
+
     }
 
 
@@ -39,18 +31,6 @@ public class SecurityConfig {
     }
 
     @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder(){
-        //비밀번호 암호화
-        return new BCryptPasswordEncoder();
-    }
-
-    //로그인이 안됐는데 로그인 필터 추가함
-    public LoginFilter jwtAuthenticationFilter() throws Exception {
-        LoginFilter filter = new LoginFilter(authenticationManagerBean(), jwtUtil, redisUtil);
-        System.out.println("로그인 필터 넘어옴");
-        return filter;
-    }
-    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable) // csrf 보안 사용 X
@@ -59,8 +39,7 @@ public class SecurityConfig {
                 .headers(h -> h
                         .frameOptions(FrameOptionsConfig::disable)
                 )
-                .httpBasic(AbstractHttpConfigurer::disable)
-                .addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+                .httpBasic(AbstractHttpConfigurer::disable);
 
         return http.build();
     }
